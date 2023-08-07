8a937cb17a0dea26ebd276864eabfe3b8ec3d044
neutrino225 <fznkrmt22503@gmail.com>
2023-08-07 22:34:22 +0500

    Added gallery scroll button

--- src/main.ts
+++ src/main.ts
@@ -99,25 +99,6 @@ elements.forEach(function (elem) {
 				}
 			);
 		},
-		onLeave: function () {
-			gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-		},
-		onEnterBack: function () {
-			gsap.fromTo(
-				elem,
-				{ y: -100, autoAlpha: 0 },
-				{
-					duration: 1.25,
-					y: 0,
-					autoAlpha: 1,
-					ease: "back",
-					overwrite: "auto",
-				}
-			);
-		},
-		onLeaveBack: function () {
-			gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-		},
 	});
 });
 
